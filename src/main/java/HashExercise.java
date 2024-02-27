643e585e4a8c2fa87cd177ba2f480b5c44f7b76f
Ellen Spertus <ellen.spertus@gmail.com>
2024-02-26 20:49:16 -0800

    Make bucket size depend on corpus size

--- src/main/java/HashExercise.java
+++ src/main/java/HashExercise.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class HashExercise {
-    public static final int NUM_BUCKETS = 50;
     private static final String RESULTS_TEMPLATE = """
             {
                 "score": 1.0,
@@ -40,6 +39,15 @@ public class HashExercise {
         }
     }
 
+    public static int simulate(int buckets) {
+        HashSet<MyString> set = new HashSet<>(buckets);
+        for (MyString line : corpus) {
+            set.add(line);
+        }
+        return set.getNumCollisions();
+    }
+
+
     public static void main(String[] args) {
         try {
             // Verify that two distinct strings with the same contents ("hello")
@@ -60,14 +68,14 @@ public class HashExercise {
                 return;
             }
 
-            // Add every line in the corpus to the hash set.
-            HashSet<MyString> set = new HashSet<>(NUM_BUCKETS);
-            for (MyString line : corpus) {
-                set.add(line);
-            }
+            // Simulate with different numbers of buckets.
+            int numCollisions = simulate(corpus.size() / 2)
+                    + simulate(corpus.size())
+                    + simulate(corpus.size() * 2)
+                    + simulate(corpus.size() * 3);
 
             // Output the results.
-            outputResults(set.getNumCollisions());
+            outputResults(numCollisions);
         } catch (Exception e) {
             // This could happen if MyString.hashCode() throws an exception.
             outputFailure("Unexpected error: " + e.getMessage());
